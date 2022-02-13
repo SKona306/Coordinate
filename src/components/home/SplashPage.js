e6e43141dd2516cacdc8d48e9c61f79edb411361
Shaun Kent <shaunkent81@gmail.com>
2022-02-12 22:00:17 -0800

    add placeholder content to test spashpage

--- src/components/home/SplashPage.js
+++ src/components/home/SplashPage.js
@@ -8,11 +8,23 @@ const SplashPage = () => {
         <div className='welcome-title'></div>
       </div>
       <div className='process-wrapper'>
-        <div className='step-one'></div>
-        <div className='step-two'></div>
-        <div className='step-three'></div>
-        <div className='step-four'></div>
-        <div className='step-five'></div>
+        <div className='step-one'>
+          <h3>Step One: Sign up for Coordinate</h3>
+        </div>
+        <div className='step-two'>
+          <h3>Step Two: Add a Trip</h3>
+        </div>
+        <div className='step-three'>
+          <h3>Step Three: Add trip details</h3>
+          <p style={{color:'lightgray'}}>(itinerary, pre-trip to-do's, activity ideas, etc.)</p>
+        </div>
+        <div className='step-four'>
+          <h3>Step Four: Invite your friends!</h3>
+        </div>
+        <div className='step-five'>
+          <h3>Step Five: Sit back and Relax</h3>
+          <p style={{color:'lightgray'}}>We'll take care of everything from here</p>
+        </div>
       </div>
     </React.Fragment>
   )
