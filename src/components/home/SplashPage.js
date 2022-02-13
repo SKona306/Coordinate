1d0ba3638095977b3e85cc00fee396d80f8027ff
Shaun Kent <shaunkent81@gmail.com>
2022-02-12 21:50:52 -0800

    change about.js to splashpage.js add splashpage structure

---
+++ src/components/home/SplashPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SplashPage = () => {
+  return (
+    <React.Fragment>
+      <div className='welcome-wrapper'>
+        <div className='background-image-wrapper'></div>
+        <div className='welcome-title'></div>
+      </div>
+      <div className='process-wrapper'>
+        <div className='step-one'></div>
+        <div className='step-two'></div>
+        <div className='step-three'></div>
+        <div className='step-four'></div>
+        <div className='step-five'></div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default SplashPage
