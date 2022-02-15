8f7a9992538a808ec9029a44407b8eac6ff24d3a
Shaun Kent <shaunkent81@gmail.com>
2022-02-14 17:08:12 -0800

    change dashboard to better logic for component switching

--- src/components/main/Dashboard.js
+++ src/components/main/Dashboard.js
@@ -5,32 +5,25 @@ import NewItineraryItemsForm from './NewItineraryItemsForm';
 const Dashboard = () => {
   const [itineraryFormPageVisible, setitineraryFormPageVisble] = useState(false);
 
-
-  const formPageVisible = () => {
-    if(itineraryFormPageVisible === true) {
-      return (
-        <NewItineraryItemsForm />
-      )
-    } else {
-      return (
-        <Itinerary />
-      )
-    }
-  }
-
   const handleAddItineraryClick = () => {
     setitineraryFormPageVisble(true);
   }
 
-
-  return (
-    <React.Fragment>
-      {formPageVisible}
-      <button className='addItineraryItem' onClick={handleAddItineraryClick}>Add Iteneray Items</button>
-      <NewItineraryItemsForm 
-        formVisibleControl = {setitineraryFormPageVisble}/>
-    </React.Fragment>
-  )
+    if(itineraryFormPageVisible === true) {
+      return (
+        <NewItineraryItemsForm 
+        formVisibleControl = {setitineraryFormPageVisble}
+        />
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <Itinerary />
+          <button className='addItineraryItem' onClick={handleAddItineraryClick}>Add Iteneray Items</button>
+        </React.Fragment>
+        
+      )
+    }
 }
 
 export default Dashboard
