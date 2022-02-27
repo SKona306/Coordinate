bc2610c3f8e319b61cde5ac62930c25cfe7c23e2
Shaun Kent <shaunkent81@gmail.com>
2022-02-27 15:32:00 -0800

    shift all content into mui components and add custom styling

--- src/components/home/SplashPage.js
+++ src/components/home/SplashPage.js
@@ -1,31 +1,40 @@
+import { Container, Grid } from '@mui/material'
 import React from 'react'
+import './SplashPage.css'
+
 
 const SplashPage = () => {
+
   return (
     <React.Fragment>
-      <div className='welcome-wrapper'>
-        <div className='background-image-wrapper'></div>
-        <div className='welcome-title'></div>
-      </div>
-      <div className='process-wrapper'>
-        <div className='step-one'>
-          <h3>Step One: Sign up for Coordinate</h3>
+      <Container>
+        <div className='lines'>
+          <h2 style={{textAlign: 'center', fontSize: '2.5rem', margin: '0.5rem'}}>How Coordinate Works</h2>
         </div>
-        <div className='step-two'>
-          <h3>Step Two: Add a Trip</h3>
+        <div className='sub-title'>
+          <h2 style={{textAlign: 'center', margin: '2rem'}}>You are just four easy steps away from relaxation!</h2>
         </div>
-        <div className='step-three'>
-          <h3>Step Three: Add trip details</h3>
-          <p style={{color:'lightgray'}}>(itinerary, pre-trip to-do's, activity ideas, etc.)</p>
+        <div className='steps-cards'>
+          <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <h1 style={{color: '#F39189'}}>Step One</h1>
+            <p>If you have an upcoming trip or are looking to plan a future trip, sign up for coordinate and let us take care of the details.</p>
+          </Grid>
+          <Grid item xs={3}>
+            <h1 style={{color: '#BB8082'}}>Step Two</h1>
+            <p>Add your trip to your account and provide us with the important information like itinerary, pre-travel todos, or even activity suggestions</p>
+          </Grid>
+          <Grid item xs={3}>
+            <h1 style={{color: '#6E7582'}}>Step Three</h1>
+            <p>Invite your friends and family to your trip, once they sign up they will be able to see all the trip info.</p>
+          </Grid>
+          <Grid item xs={3}>
+            <h1 style={{color: '#046582'}} >Step Four</h1>
+            <p>Sit back and Relax we will take care of it from here!</p>
+          </Grid>
+        </Grid>
         </div>
-        <div className='step-four'>
-          <h3>Step Four: Invite your friends!</h3>
-        </div>
-        <div className='step-five'>
-          <h3>Step Five: Sit back and Relax</h3>
-          <p style={{color:'lightgray'}}>We'll take care of everything from here</p>
-        </div>
-      </div>
+      </Container>
     </React.Fragment>
   )
 }
