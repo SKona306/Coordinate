187111e721eeb0f11ac6119ec82f25b5f9c60c7a
Shaun Kent <shaunkent81@gmail.com>
2022-03-01 16:59:36 -0800

    initialize firestore database

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBVa706_C-iXHsyUP91srpJFUQv2ce7hLc",
@@ -11,6 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export {app, auth}
+export {app, auth, db}
