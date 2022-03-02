d9e3c61ef77cd55abb58d816fe8b95e77dab91a2
Shaun Kent <shaunkent81@gmail.com>
2022-03-01 18:56:40 -0800

    add react-router-dom and routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import Signup from './components/auth/Signup';
-import Footer from './components/home/Footer';
-import Header from './components/home/Header';
 import SplashPage from './components/home/SplashPage';
+import Dashboard from './components/main/Dashboard';
 import AuthProvider from './contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/auth/Login';
 
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <Header />
-        <SplashPage />
-        {/* <Dashboard /> */}
-        <Footer />
-        <Signup />
-      </div>
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path="/" element={<SplashPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+    
   );
 }
 
