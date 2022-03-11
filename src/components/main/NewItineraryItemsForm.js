2a6d9aabc3c1ab9d9eea60f6b864d32f606e7448
Shaun Kent <shaunkent81@gmail.com>
2022-03-11 14:36:22 -0800

    change styling of newitinerary form

--- src/components/main/NewItineraryItemsForm.js
+++ src/components/main/NewItineraryItemsForm.js
@@ -1,36 +1,33 @@
-import { Button, Container, Paper, TextareaAutosize, TextField, Typography } from '@mui/material';
+import { Button, Container, Paper, TextareaAutosize, TextField, Typography, IconButton } from '@mui/material';
 import React, { useState } from 'react'
 import MainHeader from './MainHeader';
 import { itineraryItemsDatabase } from './StaticDatabase';
+import { RemoveCircle, Add, AddCircle } from '@mui/icons-material';
 
 const NewItineraryItemsForm = (props) => {
   const setFormVisible = props.formVisibleControl;
 
-  const [formValues, setFormValues] = useState([{label: '', details: '', day: '', time: '' }])
+  const [inputValues, setInputValues] = useState([{label: '', details: '', day: '', time: '' }])
 
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault()
-    formValues.forEach((element) => {
-      itineraryItemsDatabase.push(element);
-    });
-    setFormVisible(false)
+  const handleChangeInput = (index, event) => {
+    const values = [...inputValues];
+    values[index][event.target.name] = event.target.value;
+    setInputValues(values)
   }
 
-  const handleChange = (i, e) => {
-    let newFormValues = [...formValues]; 
-    newFormValues[i][e.target.name] = e.target.value; 
-    setFormValues(newFormValues);
-  }
-    
-  const addFormFields = () => {
-    setFormValues([...formValues, {label: '', details: '', day: '', time: '' }])
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValues)
   }
 
-  let removeFormFields = (i) => {
-    let newFormValues = [...formValues];
-    newFormValues.splice(i, 1);
-    setFormValues(newFormValues)
+  const handleAddFields = () => {
+    setInputValues([...inputValues, {label: '', details: '', day: '', time: '' }])
+  }
+
+  const handleRemoveFields = (index) => {
+    const values = [...inputValues];
+    values.splice(index, 1);
+    setInputValues(values);
   }
 
   const handleHomeClick = () => {
@@ -42,44 +39,61 @@ const NewItineraryItemsForm = (props) => {
       <MainHeader />
       <Paper elevation={0}>
         <Container>
-          <div className='center-paper' style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '1rem'}}>
-            <Paper>
-              <Typography variant='h1' sx={{fontSize: '2rem', textAlign: 'center'}}>
-                Itinerary Form
-              </Typography>
-              <Typography variant='h6' sx={{textAlign:'center'}}>
-                Add all your itinerary items below:
-              </Typography>
-              <Container>
-                <form onSubmit={handleFormSubmit}>
-                  {formValues.map((element, index) => (
-                    <div className="form-inline" key={index}>
-                      <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                        <TextField label='Label' sx={{margin:'1rem'}} required onChange={e => handleChange(index, e)} ></TextField>
-                        <div style={{margin:'1rem'}}>
-                          <TextareaAutosize sx={{margin:'1rem'}} placeholder='Write details here...' minRows={3.3} required onChange={e => handleChange(index, e)} ></TextareaAutosize>
-                        </div>
-                        <TextField sx={{margin:'1rem'}} type='date' label='Day' defaultValue='2021-05-22' required onChange={e => handleChange(index, e)}></TextField>
-                        <TextField sx={{margin:'1rem'}}  type='Time' label='time' defaultValue='07:30' required onChange={e => handleChange(index, e)} ></TextField>
-                      {
-                        index ? 
-                          <Button variant='contained' color='error' sx={{height:'3rem', marginTop: '1.2rem'}} onClick={() => removeFormFields(index)}>Remove</Button> 
-                        : null
-                      }
-                      </div>
-                    </div>
-                  ))}
-                  <div className='form-buttons' style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                    <Button variant='contained' color='primary' onClick={() => addFormFields()}>Add Field</Button>
-                    <Button variant='contained' color='secondary' type='submit'>Submit Itinerary</Button>
-                  </div>
-                </form>
-              </Container>
-            </Paper>
+          <Paper elevation={1} sx={{margin: '1rem'}}>
+            <Typography variant='h1' fontWeight='bolder' sx={{fontSize: '2rem', textAlign: 'center'}}>
+              Itinerary Form
+            </Typography>
+            <Typography variant='h6' sx={{textAlign: 'center'}}>
+              Add itinerary items below
+            </Typography>
+            <hr style={{maxWidth: '60vw'}}/>
+            <form onSubmit={handleSubmit} >
+            {inputValues.map((inputField, index) => (
+            <div key={index} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+              <TextField 
+                name="label"
+                label="Label"
+                variant='filled'
+                sx={{margin: '1rem' }}
+                value={inputField.label} 
+                onChange={event => handleChangeInput(index, event)}/>
+              <div className='styles-textarea' >
+                <TextareaAutosize 
+                  name="details"
+                  placeholder='Enter details here...'
+                  variant='filled'
+                  minRows={3.3}
+                  value={inputField.details} 
+                  onChange={event => handleChangeInput(index, event)}/>
+              </div>
+              <TextField 
+                name="day"
+                variant='filled'
+                type='date'
+                sx={{margin: '1rem' }}
+                value={inputField.day} 
+                onChange={event => handleChangeInput(index, event)}/>
+              <TextField 
+                name="time"
+                variant='filled'
+                sx={{margin: '0.3rem' }}
+                type='time'
+                value={inputField.time} 
+                onChange={event => handleChangeInput(index, event)}/>
+              <IconButton onClick={() => handleRemoveFields(index)}>
+                <RemoveCircle />
+              </IconButton>
+              <IconButton onClick={() => handleAddFields()}>
+                <AddCircle />
+              </IconButton>
+            </div>
+          ))}
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}} >
+            <Button variant='contained' color='primary' type='submit' endIcon={<Add />} sx={{margin: '1rem', textAlign: 'center' }} onClick={handleSubmit}>Add Items</Button>
+            <Button variant='contained' color='secondary' onClick={handleHomeClick}>Dashboard</Button>
           </div>
-          <div className='home-button' style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-            <Button variant='contained' color='success' onClick={handleHomeClick}>Dashboard</Button>
-          </div>
+          </form>
+          </Paper>
         </Container>
       </Paper>
     </React.Fragment>
