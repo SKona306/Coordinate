1ed48a2bea2d01ad9a69335e08d6ab49f136283f
Shaun Kent <shaunkent81@gmail.com>
2022-03-11 17:05:29 -0800

    add logout button to main header component

--- src/components/main/MainHeader.js
+++ src/components/main/MainHeader.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Box, Toolbar, Typography, AppBar  } from '@mui/material'
+import { Box, Toolbar, Typography, AppBar, Button } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person';
 import IconButton from '@mui/material/IconButton';
+import { useAuth } from '../../contexts/AuthContext';
 
 
 const MainHeader = (props) => {
+  const { logout } = useAuth();
+
+
+  const handleLogoutClick = async() => {
+    try {
+      await logout()
+    } catch(error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1 }}>
@@ -15,6 +27,7 @@ const MainHeader = (props) => {
               <h1 style={{fontSize: '3rem', fontFamily: "'Pacifico', cursive", padding: '0', margin:'5px'}}>Coordinate</h1>
             </div>
           </Typography>
+          <Button variant='contained' color='secondary' onClick={handleLogoutClick}>Log Out</Button>
           <IconButton
             onClick={props.profilePageVisible}>
             <PersonIcon sx={{fontSize: '40px', color: 'white'}} />
