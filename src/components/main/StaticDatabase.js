7868e5961b01f4fdf5e1a018bd150159da067218
Shaun Kent <shaunkent81@gmail.com>
2022-03-11 17:05:48 -0800

    add sample data to static database

--- src/components/main/StaticDatabase.js
+++ src/components/main/StaticDatabase.js
@@ -1,4 +1,4 @@
-let itineraryItemsDatabase = []
+let itineraryItemsDatabase = [{label: 'Hertz Rental Car', details: 'the pickup is at 9 am, $25 per day', day: '2022-05-22', time: '01:30 am' }]
 
 let toDoDatabase = []
 
