3dafddf46e0d271081f0b560530863d9a9d455b0
Shaun Kent <shaunkent81@gmail.com>
2022-03-12 12:27:00 -0800

    fix description card width issue

--- src/components/home/SplashPage.js
+++ src/components/home/SplashPage.js
@@ -20,7 +20,7 @@ const SplashPage = () => {
   return (
     <React.Fragment>
       <Header />
-      <Container style={{minHeight: '50vh'}}>
+      <Container sx={{minHeight: '50vh', fontFamily:"'Quicksand', sans-serif", fontWeight:'bold'}}>
         <div className='lines'>
           <h2 style={{textAlign: 'center', fontSize: '2.5rem', marginTop: '4rem'}}>How Coordinate Works</h2>
         </div>
@@ -30,7 +30,7 @@ const SplashPage = () => {
         <div className='steps-cards'>
           <Grid container spacing={2} style={{marginBottom: '4rem'}}>
           <Grid item xs={3}>
-            <h1 style={{color: '#F39189'}}>Step One</h1>
+            <h1 style={{color: '#F39189', fontWeight:'bolder'}}>Step One</h1>
             <p>If you have an upcoming trip or are looking to plan a future trip, sign up for coordinate and let us take care of the details.</p>
           </Grid>
           <Grid item xs={3}>
@@ -50,13 +50,13 @@ const SplashPage = () => {
       </Container>
       <div className='splash-page-main'>
         <CssBaseline />
-        <Paper className='paperBackgroundImage' square>
+        <Paper className='paperBackgroundImage' elevation={2} square sx={{width: '100vw', fontFamily: " 'Quicksand', sans-serif"}}>
           <Container className='centerElements' >
             <Paper style={{marginTop: '6rem', background: "linear-gradient(150deg, #046582, #6b73a9, #c17aa8, #f39189)"}}>
               <Container>
                 <div className='description-card'>
                   <h1 style={{marginBottom: '0.5rem'}}>Our Mission</h1>
-                  <h3 style={{width: '50%', marginTop: '0', textAlign: 'center'}}>We here at coordinate believe that life is stressful as is and you shouldn't have to worry about the details when you are one vacaction. Thats where we come in, help us help you make the most of your trip. We strive to create an experience that maximizes your total down time. </h3>
+                  <h3 style={{width: '50%', marginTop: '0'}}>We here at coordinate believe that life is stressful as is and you shouldn't have to worry about the details when you are one vacaction. Thats where we come in, help us help you make the most of your trip. We strive to create an experience that maximizes your total down time. </h3>
                 </div>
               </Container>
             </Paper>
