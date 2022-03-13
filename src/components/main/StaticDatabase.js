c88b3a16bbed02201eabac9087602058a1feff22
Shaun Kent <shaunkent81@gmail.com>
2022-03-12 20:25:29 -0800

    add logic for todo database

--- src/components/main/StaticDatabase.js
+++ src/components/main/StaticDatabase.js
@@ -1,12 +1,18 @@
 let itineraryItemsDatabase = [{label: 'Hertz Rental Car', details: 'We are renting a white nissan altima, the reservation is under James. The rental will be $27 a day', day: '2022-05-22', time: '01:30'}];
+let toDoDatabase = [{label: 'Hawaii Safe Travels', details: 'go to hawaii safe travels form to fill out application', date: '2022-05-22'}];
 
-let toDoDatabase = [];
-
-let friendsDatabase = [{name: "James", id:"012351"}, {name: "roger", id: "487945"}]
+let friendsDatabase = [{name: "James", id:"012351"}, {name: "Roger", id: "487945"}]
 
 const addItemsToItinerary = (itineraryArray) => {
-  itineraryItemsDatabase.push(itineraryArray[0])
-
+  itineraryArray.forEach(element => {
+    itineraryItemsDatabase.push(element)
+  });
 }
 
-export { itineraryItemsDatabase, toDoDatabase, friendsDatabase, addItemsToItinerary}
+const addTodoItems = (todoItemsArray) => {
+  todoItemsArray.forEach(element => {
+    toDoDatabase.push(element)
+  });
+}
+
+export { itineraryItemsDatabase, toDoDatabase, friendsDatabase, addItemsToItinerary, addTodoItems}
