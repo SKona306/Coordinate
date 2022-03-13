620e8e69ea0a685759d6a5b984628fcfc0932325
Shaun Kent <shaunkent81@gmail.com>
2022-03-12 20:23:48 -0800

    clean up itinerary component

--- src/components/main/Itinerary.js
+++ src/components/main/Itinerary.js
@@ -1,7 +1,7 @@
 import { Container, Paper, Typography, Button, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import React from 'react'
 import { itineraryItemsDatabase } from './StaticDatabase'
-import { ExpandMore } from '@mui/icons-material';
+import { ExpandMore, Add } from '@mui/icons-material';
 
 
 
@@ -15,10 +15,8 @@ const Itinerary = (props) => {
 
   return (
     <React.Fragment>
-      <Container>
-        <Paper elevation={0} sx={{margin: '1rem'}}>
-        <Container>
-          <Paper elevation={1} >
+        <Container sx={{width: '75vw', mb: 3}}>
+          <Paper elevation={5}>
             <Typography variant='h1' sx={{fontSize: '2rem', textAlign: 'center', margin:'1rem', fontFamily: " 'Quicksand', sans-serif", fontWeight: '500'}}>
               Your Itinerary
             </Typography>
@@ -30,8 +28,7 @@ const Itinerary = (props) => {
                 <AccordionSummary
                   expandIcon={<ExpandMore />}
                   aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
+                  id="panel1a-header">
                 <Typography sx={{width: '33%', flexShrink: 0}}>{item.label}</Typography>
                 <Typography sx={{fontWeight:'bold'}}>Date: {item.day}</Typography>
                 </AccordionSummary>
@@ -47,18 +44,17 @@ const Itinerary = (props) => {
             </div>
             )
           })}
-          <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Button 
-              variant='contained' 
-              onClick={handleAddItineraryClick}
-              fullWidth
-              sx={{alignItems: 'center', maxWidth: '30vw', margin:'1rem'}}
-              >Add Iteneray Items</Button>
-          </div>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <Button 
+                variant='contained' 
+                onClick={handleAddItineraryClick}
+                fullWidth
+                endIcon={<Add />}
+                sx={{alignItems: 'center', maxWidth: '30vw', margin:'1rem'}}
+                >Add Iteneray Items</Button>
+            </div>
           </Paper>
         </Container>
-      </Paper>
-      </Container>
     </React.Fragment>
   )
 }
